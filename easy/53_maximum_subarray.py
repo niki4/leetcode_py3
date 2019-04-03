68b528ab28289de974ef39a8cfb9f8db6c5d2f58
Ivan Nikiforov <nikiforr@gmail.com>
2019-04-03 13:24:25 +0300

    Update 'Maximum subarray' problem with Kadane alg.

--- easy/53_maximum_subarray.py
+++ easy/53_maximum_subarray.py
@@ -13,31 +13,28 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution using
 the divide and conquer approach, which is more subtle.
 """
-from math import floor
 
-# TODO: Complete the solution
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :param nums: List[int]
-        :return: int
-        """
-        # max_subarray = []
-
-        half_part = floor(len(nums) / 2)
-
-        while half_part > 1:
-
-            if sum(nums[:half_part]) < sum(nums[half_part:]):
-                max_subarray = nums[half_part:]
-            else:
-                max_subarray = nums[:half_part]
-
-            self.maxSubArray(max_subarray)
-
-        # return max_subarray
+    """
+    Kadane's algorithm
+    https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+    
+    Runtime: 44 ms, faster than 93.79% of Python3.
+    Memory Usage: 13.6 MB, less than 5.50% of Python3.
+    Runtime complexity: O(n)
+    """
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        curr_sum = max_sum = nums[0]
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+            
+        return max_sum
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))  # expected: 6  ([4,-1,2,1] has the largest sum = 6)
+    assert sol.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
