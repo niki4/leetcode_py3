b5515ab93bdd1dea4d2c8ad6d6ce33f16cbca629
Ivan Nikiforov <nikiforr@gmail.com>
2019-04-10 19:12:48 +0300

    Add new solution for "Two sum" problem

--- easy/001_two_sum.py
+++ easy/001_two_sum.py
@@ -10,13 +10,14 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-
-Runtime: 2228 ms  (https://leetcode.com/submissions/detail/150705433/)
-Status: Accepted (Your runtime beats 34.31 % of python3 submissions.)
 """
 
 
 class Solution:
+    """
+    Runtime: 2228 ms  (https://leetcode.com/submissions/detail/150705433/)
+    Status: Accepted (Your runtime beats 34.31 % of python3 submissions.)
+    """
     def twoSum(self, nums, target):
         """
         :param nums: List[int]
@@ -32,11 +33,32 @@ class Solution:
                         [idx for idx, val in enumerate(nums) if (val == (target - num) and idx != nums.index(num))][0]
                     ]
 
+    """
+    Runtime: 36 ms, faster than 99.34% of Python3.
+    Memory Usage: 14.9 MB, less than 5.08% of Python3.
+    """
+    def twoSum2(self, nums, target: int):
+        buffer = {}
+        for i in range(len(nums)):
+            curr = nums[i]
+            if curr in buffer:
+                return [buffer[curr], i]
+            else:
+                buffer[target - curr] = i
+
 
 if __name__ == '__main__':
-    sol = Solution()
-    print(sol.twoSum(nums=[2, 7, 11, 15], target=9))  # [0, 1]
-    print(sol.twoSum(nums=[3, 2, 4], target=6)),      # [1, 2]
-    print(sol.twoSum(nums=[3, 3], target=6))          # [0, 1]
-    # assert sol.twoSum(nums=[2, 7, 11, 15], target=9) == [0, 1]
-    # assert sol.twoSum(nums=[3, 2, 4], target=6) == [1, 2]
+    s = Solution()
+    ts = s.twoSum
+    ts2 = s.twoSum2
+    cases = (
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4],      6, [1, 2]),
+        ([3, 3],         6, [0, 1]),
+        ([3, 2, 4],      6, [1, 2]),
+        ([0, 4, 3, 0],   0, [0, 3]),
+    )
+    for case in cases:
+        nums, target, expected = case
+        assert ts(nums, target) == expected
+        assert ts2(nums, target) == expected
