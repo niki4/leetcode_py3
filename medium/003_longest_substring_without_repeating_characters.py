622ccbd0f657ce08a44648df5e68142bf80a8529
Ivan Nikiforov <nikiforr@gmail.com>
2020-10-21 18:19:31 +0300

    add new solution to "longest substring without repeating characters"

--- medium/003_longest_substring_without_repeating_characters.py
+++ medium/003_longest_substring_without_repeating_characters.py
@@ -6,15 +6,17 @@ Input: "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
 
-
-Runtime: 948 ms, faster than 7.40% of Python3 online submissions.
-Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions.
 """
 
 __author__ = 'Ivan Nikiforov'
 
 
 class Solution:
+    """
+    Runtime: 948 ms, faster than 7.40% of Python3 online submissions.
+    Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions.
+    """
+
     def lengthOfLongestSubstring(self, s: 'str') -> 'int':
         word = str()
         max_word = int()
@@ -52,16 +54,43 @@ class Solution:
         return max_word
 
 
+class Solution2:
+    """
+    Runtime:  476 ms, faster than 15.36% of Python3 online submissions.
+    Memory Usage: 14.2 MB, less than 100.00% of Python3 online submissions.
+
+    Using sliding window
+    """
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest_sstr_len = 0
+        visited = set()
+        end = 0
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                ltr = s[j]
+                if ltr in visited:
+                    visited.clear()
+                    break
+                end = j
+                visited.add(ltr)
+            longest_sstr_len = max(longest_sstr_len, len(s[i:end + 1]))
+
+        return longest_sstr_len
+
+
 if __name__ == '__main__':
-    sol = Solution()
-    assert sol.lengthOfLongestSubstring("abcabcbb") == 3
-    assert sol.lengthOfLongestSubstring("bbbbb") == 1
-    assert sol.lengthOfLongestSubstring("pwwkew") == 3
-    assert sol.lengthOfLongestSubstring("aab") == 2
-    assert sol.lengthOfLongestSubstring("dvdf") == 3
-    assert sol.lengthOfLongestSubstring("anviaj") == 5
-    assert sol.lengthOfLongestSubstring("tvqnkvovks") == 5
-    assert sol.lengthOfLongestSubstring("") == 0
-    assert sol.lengthOfLongestSubstring("cekwrebvhvtlesh") == 7
-    assert sol.lengthOfLongestSubstring("gsqygebs") == 6
-    assert sol.lengthOfLongestSubstring(" ") == 1
+    solutions = [Solution(), Solution2()]
+    for sol in solutions:
+        assert sol.lengthOfLongestSubstring("abcabcbb") == 3
+        assert sol.lengthOfLongestSubstring("bbbbb") == 1
+        assert sol.lengthOfLongestSubstring("pwwkew") == 3
+        assert sol.lengthOfLongestSubstring("aab") == 2
+        assert sol.lengthOfLongestSubstring("dvdf") == 3
+        assert sol.lengthOfLongestSubstring("anviaj") == 5
+        assert sol.lengthOfLongestSubstring("tvqnkvovks") == 5
+        assert sol.lengthOfLongestSubstring("") == 0
+        assert sol.lengthOfLongestSubstring("cekwrebvhvtlesh") == 7
+        assert sol.lengthOfLongestSubstring("gsqygebs") == 6
+        assert sol.lengthOfLongestSubstring(" ") == 1
