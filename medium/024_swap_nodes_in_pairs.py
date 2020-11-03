41ef22a3f856a7093756ec9bee41b4b2dd342d8b
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-03 13:13:56 +0300

    Update 024_swap_nodes_in_pairs.py
    add LC solutions

--- medium/024_swap_nodes_in_pairs.py
+++ medium/024_swap_nodes_in_pairs.py
@@ -25,3 +25,66 @@ class Solution:
             return temp
         else:
             return head
+
+        
+class Solution2:
+    """
+    LC recursion solution.
+    Runtime: 24 ms, faster than 95.14% of Python3.
+    Memory Usage: 14.1 MB, less than 99.98% of Python3.
+    
+    Time Complexity: O(N) where N is the size of the linked list.
+    Space Complexity: O(N) stack space utilized for recursion.
+    """
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        # nodes to be swapped
+        first = head
+        second = head.next
+        
+        # swapping
+        first.next = self.swapPairs(second.next)
+        second.next = first
+        
+        # now the head is the swapping node
+        return second
+
+    
+class Solution3:
+    """
+    LC iterative solution.
+    Runtime: 28 ms, faster than 83.37% of Python3.
+    Memory Usage: 14 MB, less than 99.98% of Python3.
+        
+    Time Complexity: O(N) where N is the size of the linked list.
+    Space Complexity: O(1).
+    """
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        # dummy node acts as a prevNode for the head node 
+        # of the list and hence and hence stores pointer to the head node.
+        dummy = ListNode(-1)
+        dummy.next = head
+        
+        prev_node = dummy
+        
+        while head and head.next:
+            # nodes to be swapped
+            first = head
+            second = head.next
+            
+            # swapping
+            prev_node.next = second
+            first.next = second.next
+            second.next = first
+            
+            # reinit the head and prev_node for the next swap
+            prev_node = first
+            head = first.next
+        
+        # return new head node
+        return dummy.next
