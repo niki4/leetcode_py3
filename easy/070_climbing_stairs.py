ad248d83ce123f86a1e41ce79905448177e0a480
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-03 17:10:14 +0300

    Update 070_climbing_stairs.py
    add memoization approach using regular dict cache

--- easy/070_climbing_stairs.py
+++ easy/070_climbing_stairs.py
@@ -23,11 +23,33 @@ class Solution:
     """
     Runtime: 36 ms, faster than 73.18% of Python3.
     Memory Usage: 13.2 MB, less than 5.18% of Python3.
+    Timeit: 95.3 ns ± 0.489 ns per loop (mean ± std. dev. of 7 runs, 10000000 loops each)
     """
 
     @functools.lru_cache()
     def climbStairs(self, n: int) -> int:
         return self.climbStairs(n - 1) + self.climbStairs(n - 2) if n > 2 else 2 if n == 2 else 1
+
+    
+class Solution2:
+    """
+    Actually, similar approach to the Solution one, but with regular dict instead lru_cache().
+    LC stat is floating run-to-run so I've added `%timeit climbStairs(10)` comparison result as well.
+    
+    Runtime: 24 ms
+    Memory Usage: 14.1 MB
+    Timeit: 2.48 µs ± 27.6 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    """
+    def climbStairs(self, n: int) -> int:
+        cache = {2: 2, 1: 1}
+        def climb_rec(N):
+            if N in cache:
+                return cache[N]
+            res = climb_rec(N-1) + climb_rec(N-2)
+            cache[N] = res
+            return res
+        return climb_rec(n)
+
 
 
 if __name__ == '__main__':
