b48a81fa0fcba1eaf0f3b172f4063f6da9d48cf5
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-05 18:28:17 +0300

    add iterative approach

--- easy/021_merge_two_sorted_lists.py
+++ easy/021_merge_two_sorted_lists.py
@@ -6,9 +6,6 @@ Example:
 
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
-
-Runtime: 48 ms (Your runtime beats 97.38 % of python3 submissions.)
-Status: Accepted (https://leetcode.com/submissions/detail/149845717/)
 """
 
 
@@ -25,6 +22,11 @@ class Solution:
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
+
+        Recursive approach
+        Runtime: 28 ms, faster than 97.91% of Python3.
+        Memory Usage: 14.2 MB, less than 100.00% of Python3.
+        Status: Accepted (https://leetcode.com/submissions/detail/149845717/)
         """
         if l1 and l2:
             if l1.val > l2.val:
@@ -33,8 +35,34 @@ class Solution:
         return l1 or l2
 
 
+class Solution2:
+    """
+    Iterative LC approach
+    Runtime: 40 ms, faster than 43.70% of Python3.
+    Memory Usage: 14.1 MB, less than 100.00% of Python3.
+    """
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prevhead = ListNode(-1)
+
+        prev = prevhead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        prev.next = l1 or l2
+        return prevhead.next
+
+
 if __name__ == '__main__':
-    sol = Solution()
+    sol1 = Solution()
+    sol2 = Solution2()
     l1 = ListNode([1, 2, 3])
     l2 = ListNode([4, 5, 6])
-    print(sol.mergeTwoLists(l1, l2))
+    print(sol1.mergeTwoLists(l1, l2))
+    print(sol2.mergeTwoLists(l1, l2))
