939e66ed7804c0cc14864b39d702a1fca1f0403b
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-13 17:47:26 +0300

    Add new solution to Plus One problem

--- easy/066_plus_one.py
+++ easy/066_plus_one.py
@@ -15,9 +15,15 @@ Explanation: The array represents the integer 123.
 Runtime: 40 ms  (Your runtime beats 95.20 % of python3 submissions.)
 Status: Accepted  (https://leetcode.com/submissions/detail/150407185/)
 """
+from typing import List
 
 
 class Solution:
+    """
+    Now LC requires handle leading zero case, so for input [0,0] return [0,1].
+    This breaks this solution, so I left it just for info.
+    """
+
     def plusOne(self, digits):
         """
         :type digits: List[int]
@@ -27,8 +33,31 @@ class Solution:
         return [int(x) for x in str(int_num)]
 
 
+class Solution2:
+    """
+    Runtime: 32 ms, faster than 62.19% of Python3
+    Memory Usage: 14 MB, less than 78.76% of Python3
+
+    This solution correctly handles leading zero case.
+    """
+
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1  # increase the rightmost non-nine digit
+                return digits
+
+        # case all digits are nines
+        return [1] + digits
+
+
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.plusOne([1, 2, 3]))   # expected: [1, 2, 4]
-    print(sol.plusOne([9]))         # expected: [1, 0]
-    print(sol.plusOne([9, 9]))      # expected: [1, 0, 0]
+    print(sol.plusOne([1, 2, 3]))  # expected: [1, 2, 4]
+    print(sol.plusOne([9]))  # expected: [1, 0]
+    print(sol.plusOne([9, 9]))  # expected: [1, 0, 0]
+    print(sol.plusOne([0, 0]))  # [1], however LC now need [0,1]
