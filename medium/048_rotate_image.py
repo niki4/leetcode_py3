8d4687a8f4245f4d33294065343bca56af836f25
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-17 17:49:13 +0300

    Add transpose&reverse approach

--- medium/048_rotate_image.py
+++ medium/048_rotate_image.py
@@ -28,15 +28,41 @@ class Solution:
             matrix[i] = list(reversed(v))
 
 
+class Solution2:
+    """
+    Transpose and then reverse
+
+    Runtime: 36 ms, faster than 55.34% of Python3
+    Memory Usage: 14 MB, less than 86.72% of Python3
+
+    Time complexity: O(N**2)
+    Space complexity: O(1)
+    """
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # matrix.reverse() before transpose; or after - matrix[i].reverse()
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            matrix[i].reverse()
+
+
 if __name__ == "__main__":
-    tc = [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
-        ([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]],
-         [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]),
-        ([[1]], [[1]]),
-        ([[1, 2], [3, 4]], [[3, 1], [4, 2]]),
-    ]
-    sol = Solution()
-    for matrix, expected in tc:
-        sol.rotate(matrix)
-        assert matrix == expected, f'got: {matrix}\nwant: {expected}'
+    def get_tc():
+        return [
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
+            ([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]],
+             [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]),
+            ([[1]], [[1]]),
+            ([[1, 2], [3, 4]], [[3, 1], [4, 2]]),
+        ]
+
+
+    solutions = [Solution(), Solution2()]
+    for sol in solutions:
+        for matrix, expected in get_tc():
+            sol.rotate(matrix)
+            assert matrix == expected, f'got: {matrix}\nwant: {expected}'
