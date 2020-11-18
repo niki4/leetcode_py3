079ac6af0996809655e9e51ea55e65f2a64a3325
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-18 18:37:48 +0300

    add re solution for Count And Say problem

--- easy/038_count_and_say.py
+++ easy/038_count_and_say.py
@@ -15,15 +15,9 @@ Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the
 count-and-say sequence.
 
 Note: Each term of the sequence of integers will be represented as a string.
-
-Example 1:
-    Input: 1
-    Output: "1"
-
-Example 2:
-    Input: 4
-    Output: "1211"
 """
+import re
+
 
 class Solution:
     """
@@ -32,6 +26,7 @@ class Solution:
 
     Algorithm idea: Use previous string value to generate a new one
     """
+
     def countAndSay(self, n: int) -> str:
         result = '1'
         for _ in range(n - 1):
@@ -39,25 +34,58 @@ class Solution:
             result = ''
             i = 0
             while i < len(prev):
-                curr = prev[i]
+                curr_digit = prev[i]
                 count = 1
                 i += 1
-                while i < len(prev) and prev[i] == curr:
+                while i < len(prev) and prev[i] == curr_digit:
                     count += 1
                     i += 1
-                result += str(count) + str(curr)
+                result += f'{count}{curr_digit}'
         return result
 
 
+class Solution2:
+    """
+    Regexp pattern matching, where we find out all those repetitive groups of digits.
+
+    Runtime: 48 ms, faster than 20.79% of Python3
+    Memory Usage: 14.5 MB, less than 6.85% of Python3
+
+    Time complexity: O(2**n) where n is the index of the desired seq.
+    https://en.wikipedia.org/wiki/Time_complexity#Superpolynomial_time
+    Space complexity: O(2**(n-1))
+    """
+
+    def countAndSay(self, n: int) -> str:
+        curr_seq = '1'
+        # match 0 to n repetitions digit
+        pattern = r'((.)\2*)'  # outer brackets stands for group1, inner for group2
+
+        for i in range(n - 1):
+            next_seq = []
+            for g1, g2 in re.findall(pattern, curr_seq):
+                # append <count, digit> pair
+                next_seq.append(str(len(g1)))
+                next_seq.append(g2)
+            # prepare next iteration
+            curr_seq = ''.join(next_seq)
+        return curr_seq
+
+
 if __name__ == "__main__":
-    s = Solution()
-    assert s.countAndSay(1) == '1'
-    assert s.countAndSay(2) == '11'
-    assert s.countAndSay(3) == '21'  # result means we seen 2 times of 1 on previous result (11)
-    assert s.countAndSay(4) == '1211' # result means we seen 1 time of 2, then 1 time of 11 of previous result (21)
-    assert s.countAndSay(5) == '111221' # on previous result (1211) we seen 1 - 1 time, then 2 also 1 time, then 1 - 2 times sequentially
-    assert s.countAndSay(6) == '312211'
-    assert s.countAndSay(7) == '13112221'
-    assert s.countAndSay(8) == '1113213211'
-    assert s.countAndSay(9) == '31131211131221'
-    assert s.countAndSay(10) == '13211311123113112211'
+    solutions = [Solution(), Solution2()]
+    tc = [
+        (1, '1'),
+        (2, '11'),
+        (3, '21'),  # result means we seen 2 times of 1 on previous result (11)
+        (4, '1211'),  # result means we seen 1 time of 2, then 1 time of 11 of previous result (21)
+        (5, '111221'),  # on previous result (1211) we saw 1 - 1 time, then 2 also 1 time, then 1 - 2 times sequentially
+        (6, '312211'),
+        (7, '13112221'),
+        (8, '1113213211'),
+        (9, '31131211131221'),
+        (10, '13211311123113112211'),
+    ]
+    for s in solutions:
+        for inp, exp in tc:
+            assert s.countAndSay(inp) == exp
