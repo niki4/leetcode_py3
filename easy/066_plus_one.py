cc0c645cd1cc34da0c01b5f23344666ea966016c
Ivan Nikiforov <nikiforr@gmail.com>
2020-11-25 13:59:43 +0300

    remove unused var

--- easy/066_plus_one.py
+++ easy/066_plus_one.py
@@ -42,8 +42,6 @@ class Solution2:
     """
 
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-
         for i in range(len(digits) - 1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
