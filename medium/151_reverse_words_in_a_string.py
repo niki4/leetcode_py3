ab7c2e16543680bd0f4f65144b6b99239ad399b2
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-05 14:39:43 +0300

    add solution with less built-ins usage

--- medium/151_reverse_words_in_a_string.py
+++ medium/151_reverse_words_in_a_string.py
@@ -23,8 +23,42 @@ class Solution:
         return " ".join(word for word in reversed(s.split(" ")) if word)
 
 
+class Solution2:
+    """
+    Solution without built-ins. Good to know for the interview.
+
+    Runtime: 48 ms, faster than 9.23% of Python3
+    Memory Usage: 14.5 MB, less than 13.74% of Python3
+    """
+
+    def reverseWords(self, s: str) -> str:
+        words = []
+        l_ptr, r_ptr = 0, 0
+
+        # split words using 2-pointer approach
+        while l_ptr < len(s) and r_ptr < len(s):
+            while l_ptr < len(s) and s[l_ptr] == " ":
+                l_ptr += 1
+            while r_ptr < len(s) and s[r_ptr] != " ":
+                r_ptr += 1
+            word = s[l_ptr:r_ptr]
+            if word:
+                words.append(word)
+            l_ptr = r_ptr + 1
+            r_ptr = l_ptr
+
+        # iterate words in reverse order and concatenate result string
+        reversed_s = ""
+        for i in range(len(words) - 1, -1, -1):
+            if not reversed_s:
+                reversed_s = words[i]
+            else:
+                reversed_s += " " + words[i]
+        return reversed_s
+
+
 if __name__ == '__main__':
-    solutions = [Solution()]
+    solutions = [Solution(), Solution2()]
     tc = [
         ("the sky is blue", "blue is sky the"),
         ("  hello world  ", "world hello"),
