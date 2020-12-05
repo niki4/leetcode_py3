25debac70fb11b5a3c3b7649acf3103a96058078
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-05 22:57:10 +0300

    add two-pointer approach to Move Zeroes

--- easy/283_move_zeroes.py
+++ easy/283_move_zeroes.py
@@ -10,12 +10,15 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 """
+from typing import List
+
 
 class Solution:
     """
     Runtime: 48 ms, faster than 86.25% of Python3.
     Memory Usage: 14.5 MB, less than 48.00% of Python3.
     """
+
     def moveZeroes(self, nums: list) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -42,22 +45,46 @@ class Solution2:
                      O(1) for push zero to the end of the list
     Space complexity:O(1) as we are not creating new list, but modifying it in place
     """
+
     def moveZeroes(self, nums: list) -> None:
-        for i in range(len(nums)-1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             if nums[i] == 0:
                 del nums[i]
                 nums.append(0)
 
 
+class Solution3:
+    """
+    Two-pointer approach. Slow pointer points to zero so that we know when to swap it with non-zero element (fast ptr).
+
+    Runtime: 40 ms, faster than 96.72% of Python3
+    Memory Usage: 15.4 MB, less than 14.79% of Python3
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+
 if __name__ == "__main__":
-    s = Solution()
-    s2 = Solution2()
+    def get_tc():
+        return [
+            ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
+            ([2, 1], [2, 1])
+        ]
 
-    src = [0,1,0,3,12]
-    exp = [1,3,12,0,0]
-    s.moveZeroes(src)
-    assert src == exp
 
-    src = [0,1,0,3,12]
-    s2.moveZeroes(src)
-    assert src == exp
+    solutions = [Solution(), Solution2(), Solution3()]
+    for s in solutions:
+        for inp, exp in get_tc():
+            s.moveZeroes(inp)
+            assert inp == exp, f'{s.__class__.__name__}: expected {exp}, got {inp}'
