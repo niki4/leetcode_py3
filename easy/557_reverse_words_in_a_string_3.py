2581dd0776a957a582893b286064faa22c4a5f6d
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-05 14:11:21 +0300

    add solution with less built-ins usage

--- easy/557_reverse_words_in_a_string_3.py
+++ easy/557_reverse_words_in_a_string_3.py
@@ -22,7 +22,41 @@ class Solution:
     #   return ' '.join(reversed(inp[::-1].split()))  # another way
 
 
+class Solution2:
+    """
+    2-pointer approach to split string by words.
+
+    Runtime: 76 ms, faster than 13.52% of Python3
+    Memory Usage: 15.1 MB, less than 11.76% of Python3
+    """
+
+    def reverseWords(self, s: str) -> str:
+        words = []
+        l_ptr, p_ptr = 0, 0
+
+        # split words
+        while l_ptr < len(s) and p_ptr < len(s):
+            while p_ptr < len(s) and s[p_ptr] != " ":
+                p_ptr += 1
+
+            words.append(s[l_ptr:p_ptr])
+            if p_ptr >= len(s) - 1:
+                break
+            p_ptr += 1
+            l_ptr = p_ptr
+
+        # reverse words and concatenate result string
+        reversed_s = ""
+        for w in words:
+            if not reversed_s:
+                reversed_s = "".join(reversed(w))
+            else:
+                reversed_s += " " + "".join(reversed(w))
+        return reversed_s
+
+
 if __name__ == "__main__":
-    s = Solution()
+    solutions = [Solution(), Solution2()]
     src = "Let's take LeetCode contest"
-    assert s.reverseWords(src) == "s'teL ekat edoCteeL tsetnoc"
+    for s in solutions:
+        assert s.reverseWords(src) == "s'teL ekat edoCteeL tsetnoc"
