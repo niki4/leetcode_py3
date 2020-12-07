67aae75416afef3b7d57c25b658712b4dba44c23
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-07 18:02:51 +0300

    Add new solution to Merge Two Sorted Lists

--- easy/021_merge_two_sorted_lists.py
+++ easy/021_merge_two_sorted_lists.py
@@ -30,7 +30,7 @@ class Solution:
         """
         if l1 and l2:
             if l1.val > l2.val:
-                l1, l2 = l2, l1
+                l1, l2 = l2, l1  # swap guarantees us l1 is always lowest (or eq) value element
             l1.next = self.mergeTwoLists(l1.next, l2)
         return l1 or l2
 
@@ -59,10 +59,71 @@ class Solution2:
         return prevhead.next
 
 
+class Solution3:
+    """
+    Bruteforce solution with O(2n+m) additional space for storing values / new list to return.
+    Maybe good starting point for further optimization / more complicated logic.
+
+    Runtime: 40 ms, faster than 38.87% of Python3
+    Memory Usage: 14.4 MB, less than 7.85% of Python3
+    """
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        list_values = list()
+        while l1:
+            list_values.append(l1.val)
+            l1 = l1.next
+        while l2:
+            list_values.append(l2.val)
+            l2 = l2.next
+        list_values.sort()
+
+        head = None
+        prev = None
+        for v in list_values:
+            node = ListNode(v)
+            if not head:
+                head = prev = node
+            else:
+                prev.next = node  # make link to the new node
+                prev = node  # remember the new node for next iteration
+        return head
+
+
 if __name__ == '__main__':
-    sol1 = Solution()
-    sol2 = Solution2()
-    l1 = ListNode([1, 2, 3])
-    l2 = ListNode([4, 5, 6])
-    print(sol1.mergeTwoLists(l1, l2))
-    print(sol2.mergeTwoLists(l1, l2))
+    solutions = [Solution(), Solution2(), Solution3()]
+
+
+    def make_linked_list_from_iterable(seq):
+        head = prev = None
+        for v in seq:
+            n = ListNode(v)
+            if not head:
+                head = prev = n
+            else:
+                prev.next = n
+                prev = n
+        return head
+
+
+    def traverse(head):
+        values = []
+        node = head
+        while node:
+            values.append(node.val)
+            node = node.next
+        return values
+
+
+    def get_tc():
+        return [(
+            make_linked_list_from_iterable([1, 2, 4]),
+            make_linked_list_from_iterable([1, 3, 4]),
+            make_linked_list_from_iterable([1, 1, 2, 3, 4, 4]),
+        )]
+
+
+    for s in solutions:
+        print()
+        for l1, l2, exp in get_tc():
+            assert traverse(s.mergeTwoLists(l1, l2)) == traverse(exp), f'{s.__class__.__name__} test failed'
