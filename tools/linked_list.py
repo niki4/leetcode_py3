ab2b626bb3dd3a33ac51b09c8b1d20a6ecd6db5b
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-10 15:21:08 +0300

    Extract linked list helpers for DRY

---
+++ tools/linked_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def get_linked_list_representation(node: ListNode) -> str:
+    list_values = list()
+    while node:
+        list_values.append(str(node.val))
+        node = node.next
+    return ' -> '.join(list_values)
+
+
+def make_linked_list_from_iterable(seq):
+    head = prev = None
+    for v in seq:
+        n = ListNode(v)
+        if not head:
+            head = prev = n
+        else:
+            prev.next = n
+            prev = n
+    return head
