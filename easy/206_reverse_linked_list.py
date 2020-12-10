bc9e5624ef96df6fe799665ad3b2dcc88028394e
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-10 14:48:32 +0300

    Add new solution to Reverse LL

--- easy/206_reverse_linked_list.py
+++ easy/206_reverse_linked_list.py
@@ -11,11 +11,13 @@ A linked list can be reversed either iteratively or recursively.
 Could you implement both?
 """
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution:
     """
@@ -27,6 +29,7 @@ class Solution:
     Runtime complexity: O(n)
     Space complexity: O(1)
     """
+
     def reverseList(self, head: ListNode) -> ListNode:
         if head is None:
             return
@@ -39,7 +42,7 @@ class Solution:
             curr.next = prev
             prev = curr
             curr = next_node
-        return prev    # the last node is the new head
+        return prev  # the last node is the new head
 
 
 class Solution2:
@@ -49,10 +52,11 @@ class Solution2:
 
     Recursive approach.
     """
+
     def reverseList(self, head):
         return self._reverse(head)
 
-    def _reverse(self, node, prev = None):
+    def _reverse(self, node, prev=None):
         if node is None:
             return prev
 
@@ -78,6 +82,7 @@ class Solution3:
     Runtime: 32 ms, faster than 86.81% of Python3
     Memory Usage: 18.7 MB, less than 7.97% of Python3 
     """
+
     def reverseList(self, head: ListNode) -> ListNode:
         if head is None or head.next is None:
             return head
@@ -85,7 +90,34 @@ class Solution3:
         head.next.next = head
         head.next = None
         return p
-    
+
+
+class Solution4:
+    """
+    Algorithm idea: keep track of original head moving toward the other end while creating a new head at each iteration.
+    So that for the original list 1->2->3->4->5->NULL with head "1" iterations will be as below
+    2->1->->3->4->5->NULL
+    3->2->1->->->4->5->NULL
+    4->3->2->1->->->->5->NULL
+    5->4->3->2->1->->->->->NULL
+
+    Runtime: 36 ms, faster than 62.97% of Python3
+    Memory Usage: 15.6 MB, less than 35.51% of Python3
+    """
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return
+
+        # head is the original head and curr_head is the new head
+        curr_head = head
+        while head and head.next:
+            node = head.next  # remember node
+            head.next = node.next  # jump original head link over node
+            node.next = curr_head
+            curr_head = node  # set new head
+        return curr_head
+
 
 if __name__ == "__main__":
     def get_linked_list_representation(node):
@@ -95,17 +127,19 @@ if __name__ == "__main__":
             node = node.next
         return ' -> '.join(list_values)
 
-    ln1 = ListNode(1)
-    ln2 = ListNode(2)
-    ln3 = ListNode(3)
-    ln1.next = ln2
-    ln2.next = ln3
 
-    s = Solution()
-    s2 = Solution2()
+    def create_linked_list() -> ListNode:
+        ln1 = ListNode(1)
+        ln2 = ListNode(2)
+        ln3 = ListNode(3)
+        ln1.next = ln2
+        ln2.next = ln3
+        return ln1
 
-    new_head = s.reverseList(ln1)
-    assert get_linked_list_representation(new_head) == '3 -> 2 -> 1'
 
-    new_head2 = s2.reverseList(new_head)
-    assert get_linked_list_representation(new_head2) ==  '1 -> 2 -> 3'
+    solutions = [Solution(), Solution2(), Solution3(), Solution4()]
+    for s in solutions:
+        head = create_linked_list()
+        assert get_linked_list_representation(head) == '1 -> 2 -> 3'
+        new_head = s.reverseList(head)
+        assert get_linked_list_representation(new_head) == '3 -> 2 -> 1'
