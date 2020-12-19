ea00ac10c08954e2ad58de09803ebb2b44f6a931
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-19 16:46:06 +0300

    Use shared TreeNode and add bottom-up approach ref

--- easy/104_maximum_depth_of_binary_tree.py
+++ easy/104_maximum_depth_of_binary_tree.py
@@ -17,18 +17,16 @@ Given binary tree [3,9,20,null,null,15,7],
    15   7
 return its depth = 3.
 """
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+from problems.tools.binary_tree import TreeNode
 
 
 class Solution:
     """
     Runtime: 44 ms, faster than 95.68% of Python3.
     Memory Usage: 15.3 MB, less than 77.99% of Python3.
+
+    Bottom-up approach:
+    https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/534/#bottom-up-solution
     """
     def maxDepth(self, root: TreeNode) -> int:
         return 1 + max(
