57c210ad06c2eeec7813a0ac886ad38b276e20da
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-22 13:24:36 +0300

    Add in-place solution and update metrics

--- easy/977_squares_of_a_sorted_array.py
+++ easy/977_squares_of_a_sorted_array.py
@@ -19,15 +19,34 @@ Note:
 
 
 class Solution:
-    def sortedSquares(self, A: list) -> list:
-        """
-        Runtime: 172 ms, faster than 67.29% of Python3.
-        Memory Usage: 15.1 MB, less than 53.36% of Python3.
+    """
+    Runtime: 212 ms, faster than 77.96% of Python3
+    Memory Usage: 16.2 MB, less than 48.59% of Python3
 
-        Simplest and pythonic solution, but not too fast.
-        Runtime complexity: O(N logN)
-        """
+    Simplest and pythonic solution, but not too fast.
+    Time complexity: O(N logN) because of sorting
+    Space complexity: O(n) for implicitly creating new list by sorted() function
+    """
+
+    def sortedSquares(self, A: list) -> list:
         return sorted([x ** 2 for x in A])
+
+
+class Solution2:
+    """
+    Runtime: 216 ms, faster than 71.54% of Python3
+    Memory Usage: 15.6 MB, less than 96.52% of Python3
+
+    Set new values and sort in place.
+    Time complexity: O(N logN)
+    Space complexity: O(1)
+    """
+
+    def sortedSquares(self, A: list) -> list:
+        for i in range(len(A)):
+            A[i] = A[i] * A[i]
+        A.sort()
+        return A
 
 
 if __name__ == "__main__":
