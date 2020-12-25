7f441a966d670017d6f6c25ac9ed128813e39d89
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-25 17:29:29 +0300

    Add binary search solution

--- medium/033_search_in_rotated_sorted_array.py
+++ medium/033_search_in_rotated_sorted_array.py
@@ -56,13 +56,51 @@ class Solution2:
         return -1
 
 
+class Solution3:
+    """
+    Algorithm idea: Define if there rotate in the list, if it's - remember rotate pivot (shift size). Normalize the list
+     then apply Binary search. If target found, return its index taking into the account the shift size.
+
+     Runtime: 36 ms, faster than 88.86% of Python3
+     Memory Usage: 14.5 MB, less than 50.70% of Python3
+
+     Time complexity: binary search takes O(log n) and O(k+n) takes slicing.
+     Space complexity: We shadow original nums slice (if there rotation) with a normalized copy, it takes O(n)
+    """
+
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        rotate_pivot = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                rotate_pivot = i
+        if rotate_pivot:
+            nums = nums[rotate_pivot:] + nums[:rotate_pivot]
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return (mid + rotate_pivot) % len(nums)
+            elif nums[mid] < target:
+                left = mid + 1
+            else:  # nums[mid] > target
+                right = mid - 1
+        return -1
+
+
 if __name__ == '__main__':
-    solutions = [Solution(), Solution2()]
+    solutions = [Solution(), Solution2(), Solution3()]
     tc = (
         ([4, 5, 6, 7, 0, 1, 2], 0, 4),
+        ([4, 5, 6, 7, 0, 1, 2], 4, 0),
+        ([4, 5, 6, 7, 0, 1, 2], 6, 2),
         ([4, 5, 6, 7, 0, 1, 2], 3, -1),
+        ([1, 2, 3, 4, 5], 2, 1),
         ([1], 0, -1),
+        ([], 0, -1),
     )
     for s in solutions:
         for inp, trg, exp in tc:
-            assert s.search(inp, trg) == exp
+            res = s.search(inp, trg)
+            assert res == exp, f'{s.__class__.__name__}: want {exp}, got {res}'
