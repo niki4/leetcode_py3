24764e9a39f3ae8173f6581d13312fb337fcdf08
Ivan Nikiforov <nikiforr@gmail.com>
2020-12-29 18:55:38 +0300

    Add couple of linear search solutions

--- medium/034_find_first_and_last_position_of_element_in_sorted_array.py
+++ medium/034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -16,8 +16,19 @@ Output: [-1,-1]
 Runtime: 40 ms, faster than 81.34% of Python3.
 Memory Usage: 13.9 MB, less than 5.06% of Python3.
 """
+from typing import List
+
 
 class Solution:
+    """
+    Binary search
+
+    Runtime: 84 ms, faster than 65.92% of Python3
+    Memory Usage: 15.3 MB, less than 54.98% of Python3
+
+    Time complexity: O(log n)
+    """
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         low, high = 0, len(nums) - 1
         while low <= high:
@@ -28,21 +39,68 @@ class Solution:
                     L -= 1
                 while R <= high and nums[R] == target:
                     R += 1
-                return [L+1, R-1]
+                return [L + 1, R - 1]
             if nums[low] <= target < nums[mid]:
                 high = mid - 1
             else:
                 low = mid + 1
         return [-1, -1]
-        
+
+
+class Solution2:
+    """
+    Linear search
+
+    Runtime: 84 ms, faster than 65.92% of Python3
+    Memory Usage: 15.4 MB, less than 34.65% of Python3
+
+    Time complexity: O(2n) as we have to traverse list twice, this turns to O(n) because 2 is constant.
+    """
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = [-1, -1]
+        for i in range(len(nums)):
+            if nums[i] == target:
+                result[0] = i
+                break
+        if result[0] != -1:
+            for j in range(len(nums) - 1, result[0] - 1, -1):
+                if nums[j] == target:
+                    result[1] = j
+                    break
+        return result
+
+
+class Solution3:
+    """
+    Linear search
+
+    Runtime: 84 ms, faster than 65.92% of Python3
+    Memory Usage: 15.5 MB, less than 34.65% of Python3
+
+    Time complexity: O(n)
+    """
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = [-1, -1]
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if result[0] == -1:
+                    result[:] = [i, i]
+                else:
+                    result[1] = i
+        return result
+
+
 if __name__ == '__main__':
-    inp1 = [5, 7, 7, 8, 8, 10]
-    inp2 = [1]
-    inp3 = [1, 2, 3]
-    
-    sol = Solution()
-    assert sol.searchRange(inp1, 8) == [3, 4]
-    assert sol.searchRange(inp1, 6) == [-1, -1]
-    assert sol.searchRange(inp2, 1) == [0, 0]
-    assert sol.searchRange(inp3, 3) == [2, 2]
-    
+    solutions = [Solution(), Solution2(), Solution3()]
+    tc = (
+        ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
+        ([5, 7, 7, 8, 8, 10], 6, [-1, -1]),
+        ([1], 1, [0, 0]),
+        ([1, 2, 3], 3, [2, 2]),
+    )
+    for s in solutions:
+        for inp, trg, exp in tc:
+            res = s.searchRange(inp, trg)
+            assert res == exp, f'{s.__class__.__name__}: for inp {inp} with trg {trg}: exp {exp}, got {res}'
