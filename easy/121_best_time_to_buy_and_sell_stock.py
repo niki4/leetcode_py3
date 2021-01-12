7eafe34be3f8d7f74ed69812af5f28abd9a5f77e
Ivan Nikiforov <nikiforr@gmail.com>
2021-01-12 17:30:16 +0300

    Add new DP solution

--- easy/121_best_time_to_buy_and_sell_stock.py
+++ easy/121_best_time_to_buy_and_sell_stock.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     """
     This solution works, but got 'Time Limit Exceeded' from Leetcode
@@ -37,8 +40,27 @@ class Solution2:
         return max_profit
 
 
+class Solution3:
+    """
+    Runtime: 68 ms, faster than 32.54% of Python3
+    Memory Usage: 15.1 MB, less than 62.41% of Python3
+
+    Dynamic Programming.
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy_price = prices[0] if prices else 0
+        for i in range(1, len(prices)):
+            max_profit = max(max_profit, prices[i] - buy_price)
+            buy_price = min(buy_price, prices[i])
+        return max_profit
+
+
 if __name__ == "__main__":
-    solutions = [Solution(), Solution2()]
+    solutions = [Solution(), Solution2(), Solution3()]
     tc = (
         ([7, 1, 5, 3, 6, 4], 5),
         ([7, 6, 4, 3, 1], 0),
