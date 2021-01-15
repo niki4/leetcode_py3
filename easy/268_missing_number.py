39573d9b389ce08a68eec54e685136980c7f033c
Ivan Nikiforov <nikiforr@gmail.com>
2021-01-15 19:02:19 +0300

    Refactor

--- easy/268_missing_number.py
+++ easy/268_missing_number.py
@@ -15,13 +15,16 @@ Your algorithm should run in linear runtime complexity.
 Could you implement it using only constant extra space complexity?
 """
 
+
 class Solution:
     """
     Runtime: 36 ms, faster than 99.10% of Python3.
     Memory Usage: 15.4 MB, less than 5.15% of Python3.
     """
+
     def missingNumber(self, nums: list) -> int:
-        return set(range(len(nums)+1)).difference(nums).pop()
+        return set(range(len(nums) + 1)).difference(nums).pop()
+
 
 class Solution2:
     """
@@ -31,6 +34,7 @@ class Solution2:
     Variation on Gauss formula (closed-form expression for the sum).
     This approach expects monotonic range of nums started from 0 with single missed num.
     """
+
     def missingNumber(self, nums: list) -> int:
         difference = 0
         for idx, val in enumerate(nums):
@@ -39,12 +43,13 @@ class Solution2:
 
 
 if __name__ == "__main__":
-    s = Solution()
-    s2 = Solution2()
-    assert s.missingNumber([3,0,1]) == 2
-    assert s.missingNumber([9,6,4,2,3,5,7,0,1]) == 8
-    assert s.missingNumber([0]) == 1
 
-    assert s2.missingNumber([3,0,1]) == 2
-    assert s2.missingNumber([9,6,4,2,3,5,7,0,1]) == 8
-    assert s2.missingNumber([0]) == 1
+    solutions = [Solution(), Solution2()]
+    tc = (
+        ([3, 0, 1], 2),
+        ([9, 6, 4, 2, 3, 5, 7, 0, 1], 8),
+        ([0], 1),
+    )
+    for s in solutions:
+        for inp, exp in tc:
+            assert s.missingNumber(inp) == exp
