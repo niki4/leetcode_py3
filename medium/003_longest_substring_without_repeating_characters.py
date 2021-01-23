7e30f2641bf76da80f2f552986e7cbe5773425e8
Ivan Nikiforov <nikiforr@gmail.com>
2021-01-23 16:04:47 +0300

    Add two more solutions

--- medium/003_longest_substring_without_repeating_characters.py
+++ medium/003_longest_substring_without_repeating_characters.py
@@ -8,8 +8,6 @@ Explanation: The answer is "abc", with the length of 3.
 
 """
 
-__author__ = 'Ivan Nikiforov'
-
 
 class Solution:
     """
@@ -97,8 +95,74 @@ class Solution3:
         return longest_sstr_len
 
 
+class Solution4:
+    """
+    Sliding window.
+        Expand substr (right end) while there's no duplicates, evaluate max_substr on each iteration; then
+        Shrink substr (left end) until no duplicates are left.
+
+    Runtime: 84 ms, faster than 36.60% of Python3
+    Memory Usage: 14.1 MB, less than 99.33% of Python3
+
+    Time complexity:  O(2n) = O(n). In the worst case each character will be visited twice by i and j.
+    Space complexity: O(min(m, n)). Same as the previous approach. We need O(k) space for the sliding window,
+    where k is the size of the Set. The size of the Set is upper bounded by the size of the string n and the size
+    of the charset/alphabet m.
+    """
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_substr = 0
+        substr = set()
+        i, j = 0, 0
+        while i < len(s) and j < len(s):
+            if s[j] not in substr:
+                substr.add(s[j])
+                max_substr = max(max_substr, len(substr))
+                j += 1
+            else:
+                substr.remove(s[i])
+                i += 1
+        return max_substr
+
+
+class Solution5:
+    """
+    Algorithm: The above solution requires at most 2n steps. In fact, it could be optimized to require only n steps.
+    Instead of using a set to tell if a character exists or not, we could define a mapping of the characters to its idx.
+    Then we can skip the characters immediately when we found a repeated character.
+
+    The reason is that if s[j] have a duplicate in the range [i, j] with index j′, we don't need to increase i
+    little by little. We can skip all the elements in the range [i, j′] and let i to be j′+1 directly.
+
+    Runtime: 64 ms, faster than 69.06% of Python3
+    Memory Usage: 14.5 MB, less than 27.15% of Python3
+
+    Time complexity: O(n). Index j will iterate n times.
+    Space complexity (HashMap): O(min(m, n)). Same as the previous approach.
+    """
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_substr = 0
+        mp = dict()  # mp stores the current index of a character
+        i = 0
+
+        for j in range(len(s)):  # try to extend the range [i, j]
+            if s[j] in mp:  # duplicate found
+                i = max(mp[s[j]], i)
+                # print(f"duplicate found {s[j]} -> i = max{(mp[s[j]], i)}:", i, "j:", j)
+            max_substr = max(max_substr, j - i + 1)
+            mp[s[j]] = j + 1
+        return max_substr
+
+
 if __name__ == '__main__':
-    solutions = [Solution(), Solution2(), Solution3()]
+    solutions = [
+        Solution(),
+        Solution2(),
+        Solution3(),
+        Solution4(),
+        Solution5(),
+    ]
     tc = (
         ("abcabcbb", 3),
         ("bbbbb", 1),
