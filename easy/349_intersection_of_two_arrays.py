391bb17576b0e23fc0fdb302363fba6f84ad2337
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-04 18:47:22 +0300

    Explain some moments

--- easy/349_intersection_of_two_arrays.py
+++ easy/349_intersection_of_two_arrays.py
@@ -24,8 +24,8 @@ class Solution:
     """
 
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1).intersection(nums2))
-        # return list(set2 & set1)
+        return list(set(nums1).intersection(nums2))  # actually, LC accept answer without converting type to list()
+        # return list(set2 & set1)  # alternatively
 
 
 class Solution2:
