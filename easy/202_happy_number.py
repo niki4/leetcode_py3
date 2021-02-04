487fccb21afb00fc1e33723c1840b835d028c1b3
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-04 18:49:12 +0300

    Refactor

--- easy/202_happy_number.py
+++ easy/202_happy_number.py
@@ -19,6 +19,7 @@ Explanation:
 12 + 02 + 02 = 1
 """
 
+
 class Solution:
     """
     Runtime: 32 ms, faster than 99.59% of Python3.
@@ -29,17 +30,18 @@ class Solution:
     an infinite loop. So a valid solution requires that all numbers
     seen on the way to reducing n to 1 be unique.
     """
+
     def isHappy(self, n: int) -> bool:
         memo = set()
         while n not in memo:
             memo.add(n)
-            n = sum(int(x)**2 for x in str(n))
+            n = sum(int(x) ** 2 for x in str(n))
         return 1 in memo
 
 
 if __name__ == "__main__":
     s = Solution()
-    assert s.isHappy(19) == True
-    assert s.isHappy(7) == True  # 7**2 = 49 -> 4**2 + 9**2 = 16 + 81 = 97 -> ...
-    assert s.isHappy(1111111) == True  # 1**2 + ... + 1**2 = 7
-    assert s.isHappy(0) == False
+    assert s.isHappy(19) is True
+    assert s.isHappy(7) is True  # 7**2 = 49 -> 4**2 + 9**2 = 16 + 81 = 97 -> ...
+    assert s.isHappy(1111111) is True  # 1**2 + ... + 1**2 = 7
+    assert s.isHappy(0) is False
