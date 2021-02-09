7f101c576b03b401484a04789b51b91599342476
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-09 18:20:04 +0300

    Refactor

--- easy/771_jewels_and_stones.py
+++ easy/771_jewels_and_stones.py
@@ -5,22 +5,25 @@ You want to know how many of the stones you have are also jewels.
 
 The letters in J are guaranteed distinct, and all characters in J and S are letters.
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
-
-Example 1:
-
-Input: J = "aA", S = "aAAbbbb"
-Output: 3
-
-
-Runtime: 36 ms, faster than 100.00% of Python3 online submissions.
-Memory Usage: 12.4 MB, less than 100.00% of Python3 online submissions.
 """
 
+
 class Solution:
-    def numJewelsInStones(self, J: 'str', S: 'str') -> 'int':
-        return sum((S.count(ltr) for ltr in J))
+    """
+    Time Complexity: O(J.length∗S.length))
+    Space Complexity: O(1) additional space complexity in Python.
+    """
+
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum((stones.count(ltr) for ltr in jewels))
 
 
 if __name__ == '__main__':
-    sol = Solution()
-    assert sol.numJewelsInStones("aA", "aAAbbbb") == 3
+    solutions = [Solution()]
+    tc = (
+        ("aA", "aAAbbbb", 3),
+        ("z", "ZZ", 0),
+    )
+    for s in solutions:
+        for inp_jewels, inp_stones, exp in tc:
+            assert s.numJewelsInStones(inp_jewels, inp_stones) == exp
