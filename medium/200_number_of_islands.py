04d006b2dc7642640b27a9fe98c953ffe288c809
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-15 18:42:53 +0300

    Refactor and add complexity description

--- medium/200_number_of_islands.py
+++ medium/200_number_of_islands.py
@@ -22,16 +22,21 @@ Input:
 Output: 3
 """
 
+
 class Solution:
     """
     Runtime: 84 ms, faster than 62.09% of Python3.
     Memory Usage: 13.8 MB, less than 83.29% of Python3.
 
-    Algorithm idea:
+    Algorithm idea (Depth-First Search):
         1. Iteratively scan each cell
         2. If found any piece of island, recursively mark all the rest parts of this island ('*')
         3. Increase found islands count by 1, then proceed with scanning grid
+
+    Time complexity: O(M×N) where M is the number of rows and N is the number of columns.
+    Space complexity: O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep.
     """
+
     def numIslands(self, grid: list) -> int:
         self.grid = grid[:]
         islands_count = 0
@@ -45,15 +50,16 @@ class Solution:
 
     def _dfs_mark_island_as_seen(self, row: int, col: int):
         if (row < 0 or
-            col < 0 or
-            row >= len(self.grid) or
-            col >= len(self.grid[0]) or
-            self.grid[row][col] != '1'): return
+                col < 0 or
+                row >= len(self.grid) or
+                col >= len(self.grid[0]) or
+                self.grid[row][col] != '1'):
+            return
         self.grid[row][col] = '*'
-        self._dfs_mark_island_as_seen(row+1, col)
-        self._dfs_mark_island_as_seen(row-1, col)
-        self._dfs_mark_island_as_seen(row, col+1)
-        self._dfs_mark_island_as_seen(row, col-1)
+        self._dfs_mark_island_as_seen(row + 1, col)
+        self._dfs_mark_island_as_seen(row - 1, col)
+        self._dfs_mark_island_as_seen(row, col + 1)
+        self._dfs_mark_island_as_seen(row, col - 1)
 
 
 if __name__ == "__main__":
