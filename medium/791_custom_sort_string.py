2d9fa8f7aaf083a3a52a06b4f528bbe13920c41c
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-18 15:55:04 +0300

    Add two more solutions and clarify complexity

--- medium/791_custom_sort_string.py
+++ medium/791_custom_sort_string.py
@@ -7,18 +7,25 @@ Return any permutation of T (as a string) that satisfies this property.
 
 Example :
 Input:
-S = "cba"
-T = "abcd"
+S =     "cba"
+T =     "abcd"
 Output: "cbad"
 Explanation:
 "a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a".
 Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
 
 Note:
+    S has length at most 26, and no character is repeated in S.
+    T has length at most 200.
+    S and T consist of lowercase letters only.
+"""
+import collections
 
-S has length at most 26, and no character is repeated in S.
-T has length at most 200.
-S and T consist of lowercase letters only.
+"""
+For all solutions
+Time Complexity:  O(S.length+T.length), the time it takes to iterate through S and T
+Space Complexity: O(T.length). 
+                  We count at most 26 different lowercase letters, but the final answer has the same length as T.
 """
 
 
@@ -28,8 +35,6 @@ class Solution:
 
     Runtime: 32 ms, faster than 61.32% of Python3
     Memory Usage: 14.4 MB, less than 22.04% of Python3
-
-    Time / Space complexity: O(n)
     """
 
     def customSortString(self, S: str, T: str) -> str:
@@ -40,7 +45,45 @@ class Solution:
         return "".join(res)
 
 
+class Solution2:
+    """
+    Optimized first solution. Now key function improved so that it returns -1 order for non intersecting keys (that
+    means all those keys will be prefixed the result sorted letters)
+
+    Runtime: 16 ms, faster than 99.85% of Python3
+    Memory Usage: 14.2 MB, less than 55.02% of Python3
+    """
+
+    def customSortString(self, S: str, T: str) -> str:
+        s_pos = {ch: i for (i, ch) in enumerate(S)}
+        return "".join(sorted(T, key=lambda t_key: s_pos.get(t_key, -1)))
+
+
+class Solution3:
+    """
+    Using counter (hash map) to count num of values per each letter, and S string as a primer.
+
+    Runtime: 28 ms, faster than 84.35% of Python3
+    Memory Usage: 14.4 MB, less than 22.04% of Python3
+    """
+
+    def customSortString(self, S: str, T: str) -> str:
+        result = ""
+        t_ctr = collections.Counter(T)
+
+        for ch in S:
+            result += ch * t_ctr[ch]  # e.g., "a" * 3 = "aaa"
+            t_ctr[ch] = 0  # discard counter for char we just processed
+
+        # add chars that are not present in S
+        for ch in t_ctr:
+            if t_ctr[ch]:
+                result += ch * t_ctr[ch]
+        return result
+
+
 if __name__ == '__main__':
-    solutions = [Solution()]
+    solutions = [Solution(), Solution2(), Solution3()]
     for sol in solutions:
-        assert sol.customSortString("cba", "abcd") == "cbad"
+        res = sol.customSortString("cba", "abcd")
+        assert res in ("cbad", "dcba", "cdba")
