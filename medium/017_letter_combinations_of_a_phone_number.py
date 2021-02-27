716c01c1de079f299b84f3d090f536a1908192d9
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-27 13:19:48 +0300

    Add solution using itertools.product

--- medium/017_letter_combinations_of_a_phone_number.py
+++ medium/017_letter_combinations_of_a_phone_number.py
@@ -9,7 +9,19 @@ Constraints:
 0 <= digits.length <= 4
 digits[i] is a digit in the range ['2', '9'].
 """
+import itertools
 from typing import List
+
+dial_pad = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
 
 
 class Solution:
@@ -24,25 +36,27 @@ class Solution:
                 and N+M is the total number digits in the input.
     Space complexity: O(3^N * 4^M) since one has to keep 3^N * 4^M solutions.
     """
+
     def letterCombinations(self, digits: str) -> List[str]:
-        dial_pad = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
-        }
         cmb = [""] if digits else []
         for d in digits:
             cmb = [p + q for p in cmb for q in dial_pad[d]]
         return cmb
 
 
+class Solution2:
+    """
+    Runtime: 20 ms, faster than 99.39% of Python3
+    Memory Usage: 14.4 MB, less than 6.93% of Python3
+    """
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        letters = [dial_pad[d] for d in digits]
+        return ["".join(cmb) for cmb in itertools.product(*letters)] if letters else []
+
+
 if __name__ == '__main__':
-    solutions = [Solution()]
+    solutions = [Solution(), Solution2()]
     tc = (
         ("23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
         ("", []),
@@ -50,4 +64,5 @@ if __name__ == '__main__':
     )
     for s in solutions:
         for inp_digits, exp in tc:
-            assert s.letterCombinations(inp_digits) == exp
+            res = s.letterCombinations(inp_digits)
+            assert res == exp, f"{s.__class__.__name__}: exp: {exp}\ngot: {res}"
