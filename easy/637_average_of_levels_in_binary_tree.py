68a8452ee42f27d51542b6a1a5a96b8507c9f10c
Ivan Nikiforov <nikiforr@gmail.com>
2021-03-09 16:14:26 +0300

    Add BFS solution and tests

--- easy/637_average_of_levels_in_binary_tree.py
+++ easy/637_average_of_levels_in_binary_tree.py
@@ -13,6 +13,7 @@ The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 
 
 Note: The range of node's value is in the range of 32-bit signed integer.
 """
+import collections
 from typing import List
 
 from problems.tools.binary_tree import TreeNode
@@ -43,3 +44,46 @@ class Solution:
     def averageOfLevels(self, root: TreeNode) -> List[float]:
         self.traverse(root, 0)
         return [sum(lvl) / len(lvl) for lvl in self.node_vals]
+
+
+class Solution2:
+    """
+    BFS (breadth-first search) - level by level (from top to bottom)
+
+    Time complexity : O(n). The whole tree is traversed at most once.
+                            Here, n refers to the number of nodes in the given binary tree.
+    Space complexity : O(m). The size of queue or temp can grow upto at most the maximum number of nodes at any level
+    in the given binary tree. Here, mm refers to the maximum number of nodes at any level in the input tree.
+
+    Runtime: 44 ms, faster than 91.77% of Python3
+    Memory Usage: 16.6 MB, less than 70.57% of Python3
+    """
+
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+        level_nums = collections.defaultdict(list)
+        level = 0
+        queue = [(root, level)]
+
+        while queue:
+            node, level = queue.pop(0)  # pop leftmost (tree node) first
+            level_nums[level].append(node.val)
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
+        return [sum(level_nums[i]) / len(level_nums[i]) for i in range(level + 1)]
+
+
+if __name__ == '__main__':
+    solutions = [Solution, Solution2]
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    expected = [3, 14.5, 11]
+    for sol in solutions:
+        res = sol().averageOfLevels(root)
+        assert res == expected, f"{sol.__class__.__name__}: expected {expected}, got {res}"
