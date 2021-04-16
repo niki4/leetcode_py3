c4da5718102831e391c6707595f08a6f2bc5d0d4
Ivan Nikiforov <nikiforr@gmail.com>
2021-04-16 16:57:41 +0300

    Add recursive + DP solutions

--- medium/062_unique_paths.py
+++ medium/062_unique_paths.py
@@ -37,6 +37,9 @@ class Solution:
 
     The problem itself is a special case of 'Delannoy number'
     (https://en.wikipedia.org/wiki/Delannoy_number)
+
+    Time complexity: O((M+N)(log(M+N)) log log(M+N)^2)
+    Space complexity: O(1)
     """
 
     def uniquePaths(self, m: int, n: int) -> int:
@@ -50,6 +53,7 @@ class Solution2:
     Memory Usage: 13.3 MB, less than 22.42% of Python3.
 
     Dynamic Programming.
+    Time/Space complexity: O(m * n)
 
     On each step use prev result to calculate possible steps. E.g., for 3x7 grid:
     In [77]: sol.uniquePaths(3, 7)
@@ -78,13 +82,60 @@ class Solution2:
         return d[-1] if m and n else 0
 
 
+class Solution3:
+    """
+    Short recursive solution. But got TLE from LC.
+
+    The idea is that from any position (starting from top left corner) we can move either down (m - 1) or right (n - 1).
+    So for any given position we can recursively calculate possible steps from there.
+    The downwards is that we repeatedly re-calculate steps for each position.
+
+    Time complexity: O(m^n)
+    """
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+
+
+class Solution4:
+    """
+    Optimized Solution3 - using DP with memoization.
+
+    Runtime: 36 ms, faster than 15.98% of Python3
+    Memory Usage: 14.3 MB, less than 38.60% of Python3
+
+    Time complexity: O(m * n)
+    Space complexity: O(m * n) for memo and recursion stack
+    """
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = dict()
+
+        def helper(m_, n_):
+            if (m_, n_) in memo:
+                return memo[(m_, n_)]
+
+            if m_ == 1 or n_ == 1:
+                return 1
+
+            res = helper(m_ - 1, n_) + helper(m_, n_ - 1)
+            memo[(m_, n_)] = res
+            return res
+
+        result = helper(m, n)
+        return result
+
+
 if __name__ == "__main__":
-    solutions = [Solution(), Solution2()]
+    solutions = [Solution(), Solution2(), Solution3(), Solution4()]
     tc = (
         (7, 3, 28),
         (3, 7, 28),
         (3, 2, 3),
         (3, 3, 6),
+        (1, 2, 1)
     )
     for sol in solutions:
         for inp_m, inp_n, exp_res in tc:
