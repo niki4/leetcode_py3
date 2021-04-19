abf4f8f84d7b13b770551584c86df28f92479e0d
Ivan Nikiforov <nikiforr@gmail.com>
2021-04-19 09:48:39 +0300

    Add some explanations on Solution2

--- medium/593_valid_square.py
+++ medium/593_valid_square.py
@@ -47,17 +47,27 @@ class Solution:
 
 class Solution2:
     """
+    Math solution from
+    https://leetcode.com/problems/valid-square/discuss/103482/Python-Straightforward-with-Explanation
+        "Suppose points ABCD have pairwise distances in sorted order S, S, S, S, S*sqrt(2), S*sqrt(2).
+         We want to show ABCD is a square. Let us call S a "small side" and S*sqrt(2) a "large side".
+         Without loss of generality, suppose AC is a large side. If BD is a large side, then AB and BC are small sides,
+         so B lies on the intersection of circles between A and C; similarly, D lies on the same intersection, and thus
+         ABCD is a square (as two different circles of the same radius only intersect in two different points)."
+
     Runtime: 36 ms, faster than 52.15% of Python3
     Memory Usage: 14.2 MB, less than 89.11% of Python3
     """
 
     def dist(self, p1: List[int], p2: List[int]) -> int:
-        return (p2[1] - p1[1]) * (p2[1] - p1[1]) + (p2[0] - p1[0]) * (p2[0] - p1[0])
+        return (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
 
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
         D = [self.dist(p1, p2), self.dist(p1, p3), self.dist(p1, p4),
              self.dist(p2, p3), self.dist(p2, p4), self.dist(p3, p4)]
         D.sort()
+        # For input [0, 0], [1, 1], [1, 0], [0, 1] result D will be [1, 1, 1, 1, 2, 2] == True
+        # For input [1, 1], [5, 3], [3, 5], [7, 7] D will be [8, 20, 20, 20, 20, 72] == False
         return 0 < D[0] == D[1] == D[2] == D[3] and 2 * D[0] == D[4] == D[5]
 
 
