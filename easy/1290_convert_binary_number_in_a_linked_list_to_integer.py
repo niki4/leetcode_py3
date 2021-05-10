2b1283f6b705f503809a8de611aaf685b1ea3897
Ivan Nikiforov <nikiforr@gmail.com>
2021-05-10 15:51:11 +0300

    Add "Convert Binary Number in a Linked List to Integer" problem solutions

--- easy/1290_convert_binary_number_in_a_linked_list_to_integer.py
+++ easy/1290_convert_binary_number_in_a_linked_list_to_integer.py
@@ -37,8 +37,62 @@ class Solution:
         return int("".join(digits), base=2)
 
 
+class Solution2:
+    """
+    We may convert binary representation to base10 by taking into account shift of each digit.
+    So that moving from right to left numerical place will adds 2**i to each bit where i started from 0 (growing by 1
+    at each shift left).
+
+    Albeit we traversing list from head toward its tail (so reading binary number from left to right), we do sum of all
+    "curr_bit_val * 2**i" so the result will be the same in both cases.
+
+    For example:
+    101 (base2) == (1 * 2**0) + (0 * 2**1) + (1 * 2**2) == (1 * 1) + (0 * 2) + (1 * 4) == 5 (base10)
+
+    Runtime: 28 ms, faster than 83.64% of Python3
+    Memory Usage: 14.3 MB, less than 38.29% of Python3
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+
+    def getDecimalValue(self, head: ListNode) -> int:
+        num = 0
+        node = head
+        while node:
+            num = num * 2 + node.val
+            node = node.next
+        return num
+
+
+class Solution3:
+    """
+    The same idea as in Solution2, but using bitwise operators which are generally faster.
+
+    Instead "num = num * 2 + x" at each node we using "num = (num << 1) | x" to get the same result.
+    Computation for conversion 101 (base2) to 5 (base10), step by step:
+    num = (0 << 1) | 1 == 0 | 1 == 1
+    num = (1 << 1) | 0 == 2 | 0 == 2
+    num = (2 << 1) | 1 == 4 | 1 == 5
+
+    Runtime: 28 ms, faster than 83.64% of Python3
+    Memory Usage: 14.1 MB, less than 88.88% of Python3
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+
+    def getDecimalValue(self, head: ListNode) -> int:
+        num = 0
+        node = head
+        while node:
+            num = (num << 1) | node.val
+            node = node.next
+        return num
+
+
 if __name__ == '__main__':
-    solutions = [Solution()]
+    solutions = [Solution(), Solution2(), Solution3()]
     tc = (
         ([1, 0, 1], 5),
         ([0], 0),
@@ -49,4 +103,5 @@ if __name__ == '__main__':
     for sol in solutions:
         for ll_values, exp_base10_int in tc:
             ll_head = make_linked_list_from_iterable(ll_values)
-            assert sol.getDecimalValue(ll_head) == exp_base10_int
+            res = sol.getDecimalValue(ll_head)
+            assert res == exp_base10_int, f"expected {exp_base10_int}, got {res}"
