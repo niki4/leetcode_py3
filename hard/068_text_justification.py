f333d87d1efb1b14fdfbb15460aeca0a0d6fd632
Ivan Nikiforov <nikiforr@gmail.com>
2021-06-18 15:13:32 +0300

    Remove some examples as they repeats in the tests

--- hard/068_text_justification.py
+++ hard/068_text_justification.py
@@ -24,31 +24,6 @@ Output:
    "justification.  "
 ]
 
-Example 2:
-Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
-Output:
-[
-  "What   must   be",
-  "acknowledgment  ",
-  "shall be        "
-]
-Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be
-left-justified instead of fully-justified. Note that the second line is also left-justified becase it contains
-only one word.
-
-Example 3:
-Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is",
-"everything","else","we","do"], maxWidth = 20
-Output:
-[
-  "Science  is  what we",
-  "understand      well",
-  "enough to explain to",
-  "a  computer.  Art is",
-  "everything  else  we",
-  "do                  "
-]
-
 Constraints:
     1 <= words.length <= 300
     1 <= words[i].length <= 20
