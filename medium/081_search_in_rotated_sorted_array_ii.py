b0bef4c969d9801218e5025c96c260d3370d9314
Ivan Nikiforov <nikiforr@gmail.com>
2022-03-09 23:09:03 +0100

    Add solution to Search in Rotated Sorted Array II

---
+++ medium/081_search_in_rotated_sorted_array_ii.py
@@ -0,0 +1,69 @@
+"""
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that
+the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+Example 1:
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+Constraints:
+    1 <= nums.length <= 5000
+    -104 <= nums[i] <= 104
+    nums is guaranteed to be rotated at some pivot.
+    -104 <= target <= 104
+
+Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates.
+Would this affect the runtime complexity? How and why?
+"""
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return True
+
+            if nums[left] < nums[mid]:
+                # target is in strictly increasing part in left subarr
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[left] > nums[mid]:
+                # target is in strictly increasing part in right subarr
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else: # cannot define subarr (dup value?)
+                left += 1
+
+        return False
+
+
+if __name__ == '__main__':
+    solutions = [Solution()]
+    tc = (
+        ([2,5,6,0,0,1,2], 0, True),
+        ([2,5,6,0,0,1,2], 3, False),
+    )
+    for s in solutions:
+        for inp, trg, exp in tc:
+            res = s.search(inp, trg)
+            assert res is exp, f'{s.__class__.__name__}: want {exp}, got {res}'
